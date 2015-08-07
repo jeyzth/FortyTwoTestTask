7415558d93e152d034c371bdfae6c703c47e4b83
E <jeyzth@gmail.com>
2015-08-07 22:11:51 +0300

    ticket:1  First commit This commit make only checking links ws Barista & my GitHub project There only made model.

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Contats(models.Model):
+    name       = models.CharField(max_length=20)
+    surname    = models.CharField(max_length=20)
+    dateofbird = models.DateField()
+    bio        = models.TextField()
+    email    = models.CharField(max_length=20)
+    jabber    = models.CharField(max_length=20)
+    skype    = models.CharField(max_length=20)
+    others      = models.TextField()
+    
+    def __unicode__(self):
+        return self.name
