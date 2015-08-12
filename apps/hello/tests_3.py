a0d036da3f8e0c68b209e8305c5a1b1ba7982afa
E <jeyzth@gmail.com>
2015-08-13 00:01:54 +0300

    Ticket :1 # Make 1 and 2 tests
    Trouble ws Test3 (Error 404)
    add tests

---
+++ apps/hello/tests_3.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+from django.test.client import Client
+
+from hello.models import Contacts
+
+
+class Tiket1TestAllFieldsOnPage(TestCase):
+    def test_get_mainpage(self):
+        """ This test must to check
+            All data of model are displayed on the main page.
+        """
+        sbio = u"Кодю на С and Shell під Linux (x86,armv7),на Delphi під Win32"
+        sothr = u"т. 7717123456 м. +77071234567 м.+380501234567"
+        new_rec = Contacts(
+            name=u"Василь",
+            surname=u"Анонімов",
+            dateofbird=u"1973-02-03",
+            bio=sbio,
+            email=u"jeyzth@gmail.com",
+            jabber=u"jeyzth@khavr.com",
+            skype=u"ghost",
+            others=sothr
+        )
+
+        new_rec.save()
+        c = Client()
+        response = c.get('http://localhost:8080')
+        ucontent = response.content.decode('utf8')
+        print "test_get_mainpage"
+        print ucontent
+        assert(ucontent.find(u"Євген") > 0)
+        assert(ucontent.find(u"Анонімов") > 0)
+        assert(ucontent.find(u"3 лютого 1973 р.") > 0)
+        assert(ucontent.find(sbio) > 0)
+        assert(ucontent.find(u"jeyzth@gmail.com") > 0)
+        assert(ucontent.find(u"jeyzth@khavr.com") > 0)
+        assert(ucontent.find(u"ghost") > 0)
+        assert(ucontent.find(sothr) > 0)
